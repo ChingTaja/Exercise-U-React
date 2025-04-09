805c6ea9d580b43269ecd5d49b19af32c35d50cc
Taja <101925679+antiching@users.noreply.github.com>
2025-04-10 01:00:27 +0800

    feat: player ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import Player from './component/Player';
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="player1" symbol="o" />
+          <Player initialName="player2" symbol="x" />
+        </ol>
+        Game Board
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/component/Player.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+
+  const [isEditing, setIsEditing] = useState(false);
+  const edit = () => {
+    setIsEditing((editing) => !editing);
+  };
+
+  const handleChange = (event) => {
+    setPlayerName(event.target.value);
+  };
+  return (
+    <li>
+      <span className="player">
+        {isEditing ? (
+          <input type="text" required value={playerName} onChange={handleChange} />
+        ) : (
+          <span className="player-name">{playerName}</span>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={edit}>{isEditing ? 'Save' : 'Edit'}</button>
+    </li>
+  );
+}
