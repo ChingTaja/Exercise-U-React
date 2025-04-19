c7534d1dd072ebec394973eb6b1def75ad432b77
Taja <taja526526@gmail.com>
2025-04-19 10:00:31 +0800

    feat: Investment Header.jsx

--- src/component/investment/Home.jsx
+++ src/component/investment/Home.jsx
@@ -1,4 +1,23 @@
+import '../../css/investment.css'
+import Header from './Header';
+
 export default function Investment() {
-    console.log('click!!')
-    return <>Investment Page! Investment Page!Investment Page!Investment Page!Investment Page!Investment Page!Investment Page!</>
+    return <>
+        <Header/>
+        <div className='input-group'>
+            <div id="user-input">
+                <label>Initial Investment</label>
+                <input type="text" name="" />
+
+                <label>Annual Investment</label>
+                <input type="text" name="" />
+                
+                <label>Expected return</label>
+                <input type="text" name="" />
+
+                <label>Duration</label>
+                <input type="text" name="" />
+            </div>
+        </div>
+    </>
 }
