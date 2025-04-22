24d36407d7aab074c331b666ac31876ce4770f1e
Taja <taja526526@gmail.com>
2025-04-22 09:06:52 +0800

    feat: Result

--- src/component/investment/Home.jsx
+++ src/component/investment/Home.jsx
@@ -1,12 +1,30 @@
 import '../../css/investment.css';
 import Header from './Header';
 import UserInput from './UserInput';
+import Result from './Result';
+
+const [userInput, setUserInput] = useState({
+  initialInvestment: 1000,
+  annualInvestment: 500,
+  expectedReturn: 10,
+  duration: 2,
+});
+
+const handleChange = (inputIdentifier, newValue) => {
+  setUserInput((preInput) => {
+    return {
+      ...preInput,
+      [inputIdentifier]: newValue,
+    };
+  });
+};
 
 export default function Investment() {
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput input={userInput} onChange={handleChange} />
+      <Result input={userInput} />
     </>
   );
 }
