c21e8edb9f075d5bcc7d1a06049c8a4acef23adf
Taja <taja526526@gmail.com>
2025-04-24 00:42:02 +0800

    feat: css tutorial init

---
+++ src/component/cssTutorial/Home.jsx
@@ -0,0 +1,15 @@
+import '../../css/cssTutorial.css';
+
+import AuthInputs from './AuthInputs';
+import Header from './Header.jsx';
+
+export default function CssTutorial() {
+  return (
+    <>
+      <Header />
+      <main>
+        <AuthInputs />
+      </main>
+    </>
+  );
+}
