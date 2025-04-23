2a5e9961ae1f13910f9585cdf1d1a53806d4751b
Taja <101925679+antiching@users.noreply.github.com>
2025-04-24 00:42:02 +0800

    feat: css tutorial init

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 // import TicTacToe from './component/ticTacToe/Home'
-import Investment from './component/investment/Home'
+// import Investment from './component/investment/Home'
+import CssTutorial from './component/cssTutorial/Home';
 
 function App() {
-  return (<Investment />)
+  return <CssTutorial />;
 }
 
 export default App;
