8192d9c1c2c365c61985658fe4a325b9b891125a
Taja <101925679+antiching@users.noreply.github.com>
2025-04-23 22:04:33 +0800

    feat: Result

--- src/component/investment/Home.jsx
+++ src/component/investment/Home.jsx
@@ -1,25 +1,26 @@
 import '../../css/investment.css';
+import { useState } from 'react';
 import Header from './Header';
 import UserInput from './UserInput';
 import Result from './Result';
 
-const [userInput, setUserInput] = useState({
-  initialInvestment: 1000,
-  annualInvestment: 500,
-  expectedReturn: 10,
-  duration: 2,
-});
-
-const handleChange = (inputIdentifier, newValue) => {
-  setUserInput((preInput) => {
-    return {
-      ...preInput,
-      [inputIdentifier]: newValue,
-    };
-  });
-};
-
 export default function Investment() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 1000,
+    annualInvestment: 500,
+    expectedReturn: 10,
+    duration: 2,
+  });
+
+  const handleChange = (inputIdentifier, newValue) => {
+    setUserInput((preInput) => {
+      return {
+        ...preInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  };
+
   return (
     <>
       <Header />
