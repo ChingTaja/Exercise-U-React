eb98496e285fa6e35d600d5a294a9376bff42ae0
Taja <101925679+antiching@users.noreply.github.com>
2025-04-30 21:30:51 +0800

    refactor: input and button using tailwind

--- src/component/cssTutorial/Header.jsx
+++ src/component/cssTutorial/Header.jsx
@@ -2,12 +2,12 @@ import logo from '../../assets/logo.png';
 
 export default function Header() {
   return (
-    <header className="flex flex-col items-center mt-8 mb-16">
+    <header className="flex flex-col items-center mt-8 mb-8 md:mb-16">
       <img src={logo} alt="A canvas" className="object-contain mb-8 w-44 h-44" />
-      <h1 className="text-4xl font-semibold text-center uppercase text-amber-800  tracking-widest font-title">
+      <h1 className="text-xl md:text-4xl font-semibold text-center uppercase text-amber-800  tracking-widest font-title">
         ReactArt
       </h1>
-      <p className='text-stone-500'>A community of artists and art-l overs.</p>
+      <p className="text-stone-500">A community of artists and art-l overs.</p>
     </header>
   );
 }
