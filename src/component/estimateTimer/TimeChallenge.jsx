15bf41d73694d89df4e35f488395fbd2654bd17c
Taja <taja526526@gmail.com>
2025-05-08 01:47:40 +0800

    feat: timer start

--- src/component/estimateTimer/TimeChallenge.jsx
+++ src/component/estimateTimer/TimeChallenge.jsx
@@ -1,14 +1,30 @@
+import { useState } from 'react';
+
 export default function TimeChallenge({ title, targetTime }) {
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+  const handleStart = () => {
+    setTimerStarted(true);
+    setTimeout(() => {
+      setTimerExpired(true);
+    }, 1000 * targetTime);
+  };
+
+  const handleStop = () => {};
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p> You lost!</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? 's' : ''}
       </p>
       <p>
-        <button>Start Challenge</button>
+        <button onClick={handleStart}>{timerStarted ? 'Stop' : 'Start'} Challenge</button>
       </p>
-      <p>Time is running/ ... Time inactive</p>
+      <p className={setTimerStarted ? 'active' : undefined}>
+        {setTimerStarted ? 'Timer is running' : 'Timer inactive'}
+      </p>
     </section>
   );
 }
