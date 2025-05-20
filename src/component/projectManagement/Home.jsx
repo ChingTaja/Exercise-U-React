a8c515c3a1071527a75aaf15b687a20246fbe927
Taja <taja526526@gmail.com>
2025-05-21 01:17:49 +0800

    feat: start create project

--- src/component/projectManagement/Home.jsx
+++ src/component/projectManagement/Home.jsx
@@ -1,12 +1,34 @@
 import Sidebar from './Sidebar';
 import NewProject from './NewProject';
 import NoProjectSelected from './NoProjectSelected';
+import { useState } from 'react';
 
 function App() {
+  let [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    project: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectState((preState) => {
+      return {
+        ...preState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+  if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  } else if (projectState.selectedProjectId === null) {
+    content = <NewProject />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-      <NoProjectSelected />
+      <Sidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
