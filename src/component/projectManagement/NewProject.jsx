6612f8a46fdf03029d837cc1303f2ee91c4ce489
Taja <101925679+antiching@users.noreply.github.com>
2025-05-24 13:31:17 +0800

    feat: validation and cancel

--- src/component/projectManagement/NewProject.jsx
+++ src/component/projectManagement/NewProject.jsx
@@ -1,10 +1,13 @@
 import Input from './Input';
+import Modal from './Modal';
 import { useRef } from 'react';
 
-export default function NewProject({ onAdd }) {
+export default function NewProject({ onAdd, onCancel }) {
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
+
+  const modal = useRef();
 
   function handleSave() {
     let enteredTitle = title.current.value;
@@ -12,7 +15,11 @@ export default function NewProject({ onAdd }) {
     let enteredDueDate = dueDate.current.value;
 
     // validation
-    //..
+    if (enteredTitle.trim() === '' || enteredDescription.trim() === '' || enteredDueDate.trim() === '') {
+      modal.current.open();
+
+      return;
+    }
 
     onAdd({
       title: enteredTitle,
@@ -22,22 +29,34 @@ export default function NewProject({ onAdd }) {
   }
 
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">Cancel</button>
-        </li>
-        <li>
-          <button className="bg-stone-800 text-stone-50 hover:text-stone-950 px-6 py-2 rounded-md" onClick={handleSave}>
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input type="text" ref={title} label="Title" />
-        <Input ref={description} label="Description" textarea />
-        <Input type="date" ref={dueDate} label="Due Date" />
+    <>
+      <Modal ref={modal} buttonCaption="Close">
+        <h2 className="text-xl font-bold text-stone-700 my-4">Invalud Input</h2>
+        <p className="text-stone-600 mb-4">Oops... looks like your forget to enter a value</p>
+        <p className="text-stone-600 mb-4">Please make sure you provide a valid for every input field</p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950" onClick={onCancel}>
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              className="bg-stone-800 text-stone-50 hover:text-stone-950 px-6 py-2 rounded-md"
+              onClick={handleSave}
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" ref={title} label="Title" />
+          <Input ref={description} label="Description" textarea />
+          <Input type="date" ref={dueDate} label="Due Date" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
