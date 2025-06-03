41cfa802df1ad66d5fc5241a5fba95f90ccf605e
Taja <taja526526@gmail.com>
2025-06-03 14:28:28 +0800

    feat: pick picture init

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 // import './css/estimateTimer.css';
 // import './css/projectManagement.css';
-import './css/shoppingCart.css';
+// import './css/shoppingCart.css';
+import './css/pickPic.css';
 
 // import TicTacToe from './component/ticTacToe/Home'
 // import Investment from './component/investment/Home'
 // import CssTutorial from './component/cssTutorial/Home';
 // import EstimateTimer from './component/estimateTimer/Home';
 // import ProjectManagement from './component/projectManagement/Home';
-import ShoppingCart from './component/shoppingCart/Home';
+// import ShoppingCart from './component/shoppingCart/Home';
+import PickPicture from './component/pickPic/Home';
 
 function App() {
-  return <ShoppingCart />;
+  return <PickPicture />;
 }
 
 export default App;
