1a952e656b7a391c2ca5766288884e7baac350d3
Taja <taja526526@gmail.com>
2025-06-08 16:25:43 +0800

    refactor: Question and Answer

---
+++ src/component/quiz/Question.jsx
@@ -0,0 +1,47 @@
+import QuestionTimer from './QuestionTimer.jsx';
+import Answers from './Answers.jsx';
+import { useState } from 'react';
+import QUESTIONS from './question';
+
+export default function Question({ index, onSelectAnswer, onSkipAnswer }) {
+  const [answer, setAnswer] = useState({ selectedAnswer: '', isCorrect: null });
+
+  function handleSelectAnswer(answer) {
+    setAnswer({
+      selectedAnswer: answer,
+      isCorrect: null,
+    });
+
+    setTimeout(() => {
+      setAnswer({
+        selectedAnswer: answer,
+        // 第一個是正確答案
+        isCorrect: QUESTIONS[index].answers[0] === answer,
+      });
+
+      setTimeout(() => {
+        onSelectAnswer(answer);
+      }, 2000);
+    }, 1000);
+  }
+
+  let answerState = '';
+  if (answer.selectedAnswer && answer.isCorrect !== null) {
+    answerState = answer.isCorrect ? 'correct' : 'wrong';
+  } else if (answerState.selectedAnswer) {
+    answerState = 'answered';
+  }
+
+  return (
+    <div id="question">
+      <QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
+      <h2>{QUESTIONS[index].text}</h2>
+      <Answers
+        answers={QUESTIONS[index].answers}
+        selectedAnswer={answer.selectedAnswer}
+        answerState={answerState}
+        onSelect={handleSelectAnswer}
+      />
+    </div>
+  );
+}
