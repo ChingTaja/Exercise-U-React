5a2e9115f64ba606840884ad916fd35837d02c46
Taja <101925679+antiching@users.noreply.github.com>
2025-06-10 11:08:10 +0800

    feat: counter init

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@
 // import './css/projectManagement.css';
 // import './css/shoppingCart.css';
 // import './css/pickPic.css';
-import './css/quiz.css';
+// import './css/quiz.css';
+import './css/counter.css';
 
 // import TicTacToe from './component/ticTacToe/Home'
 // import Investment from './component/investment/Home'
@@ -11,10 +12,11 @@ import './css/quiz.css';
 // import ProjectManagement from './component/projectManagement/Home';
 // import ShoppingCart from './component/shoppingCart/Home';
 // import PickPicture from './component/pickPic/Home';
-import QuizHome from './component/quiz/Home';
+// import QuizHome from './component/quiz/Home';
+import Counter from './component/counter/Home';
 
 function App() {
-  return <QuizHome />;
+  return <Counter />;
 }
 
 export default App;
