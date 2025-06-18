c55752d591ed30b8f479602c71b9f3ebdbe98387
Taja <taja526526@gmail.com>
2025-06-18 21:57:36 +0800

    feat: counter useSelector

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import { StrictMode } from 'react';
-
+import { Provider } from 'react-redux';
 import App from './App.jsx';
+import store from './component/counter-redux/store/index.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
