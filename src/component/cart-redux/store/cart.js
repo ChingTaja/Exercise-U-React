25a1554b85f4703e0af829c3db153961af20b02d
Taja <taja526526@gmail.com>
2025-06-25 00:46:28 +0800

    fix: product and cart item

--- src/component/cart-redux/store/cart.js
+++ src/component/cart-redux/store/cart.js
@@ -9,14 +9,13 @@ const cartSlice = createSlice({
   reducers: {
     addItemToCart(state, action) {
       const newItem = action.payload;
-      // FIXME: itemId ?? id ??
 
       state.totalQuantity++;
-      const existingItem = state.items.find((item) => item.itemId === newItem.id);
+      const existingItem = state.items.find((item) => item.id === newItem.id);
 
       if (!existingItem) {
         state.items.push({
-          itemId: newItem.id,
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
